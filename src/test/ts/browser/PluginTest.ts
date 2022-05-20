fe61f789e8eea3317830ee1aa9583997d277e082
Ivan <jerzabek.ivan@gmail.com>
2022-05-20 12:04:07 +0200

    Init main tinymce plugin

---
+++ src/test/ts/browser/PluginTest.ts
@@ -0,0 +1,17 @@
+import { TinyAssertions, TinyHooks, TinyUiActions } from '@ephox/mcagar';
+
+import Plugin from '../../../main/ts/Plugin';
+
+// This an example of a browser test of the editor.
+describe('browser.PluginTest', () => {
+  const hook = TinyHooks.bddSetup({
+    plugins: 'zavrad',
+    toolbar: 'zavrad'
+  }, [ Plugin ]);
+
+  it('test click on button', () => {
+    const editor = hook.editor();
+    TinyUiActions.clickOnToolbar(editor, 'button:contains("zavrad button")');
+    TinyAssertions.assertContent(editor, '<p>content added from zavrad</p>');
+  });
+});
